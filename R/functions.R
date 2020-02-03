62d634f325c9543f0361b58b3c474fdc8318badc
Francescolaz <60002222+Francescolaz@users.noreply.github.com>
2020-02-03 13:44:30 +0100

    Update functions.R

--- R/functions.R
+++ R/functions.R
@@ -8,6 +8,7 @@
 #'
 #' @examples
 #' import_data()
+#' @export
 
 import_data <- function(){
   dataset_df<-read.csv('https://raw.githubusercontent.com/unimi-dse/9768672c/master/data/Tesla%20top%205.csv', sep = ',')
