4e094177232338bc4f922814594a17586b146b7e
Francescolaz <60002222+Francescolaz@users.noreply.github.com>
2020-02-08 23:19:43 +0100

    Add files via upload

--- R/data.R
+++ R/data.R
@@ -1,8 +1,15 @@
 #' Sales of Testla Motorcars in the top-5 European Countries during a period from the third quarter
 #' of 2013 to the fourth quarter of 2019.
 #'
-#' A dataset that contains real value-data for statistical porpuses with the table data can be found at this url.link:
-#' https://teslamotorsc2013-0-01lub.com/tmc/threads/tesla-europe-registration-stats.61651/.
+#' A dataset that contains real value-data for statistical porpuses.
 #'
-#' @format A data frame with 26 rows and 5 variables
+#' @format A data frame with 26 rows and 5 variables:
+#' \describe{
+#'   \item{Nor}{Observations of the sales of Tesla during the period considered in Norway}
+#'   \item{Ned}{Observations of the sales of Tesla during the period considered in Nederlands}
+#'   \item{Swi}{Observations of the sales of Tesla during the period considered in Switzerland}
+#'   \item{Ger}{Observations of the sales of Tesla during the period considered in Germany}
+#'   \item{UK}{Observations of the sales of Tesla during the period considered in United Kingdom}
+#' }
+#' @source \url{https://teslamotorsc2013-0-01lub.com/tmc/threads/tesla-europe-registration-stats.61651/}
 "dataset"
